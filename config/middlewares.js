486b10075e59847426aa1b1e7c833ecf1c73a3f7
Simon Papenberg <simon.papenberg+muglobal@multivac.de>
2026-04-10 08:05:39 +0200

    change CORS for AI Studio development

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,15 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['*'], // Erlaubt Anfragen von allen Domains (wichtig für AI Studio/Stitch)
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+      keepHeaderOnError: true,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
